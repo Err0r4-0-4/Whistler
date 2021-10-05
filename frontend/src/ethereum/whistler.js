18f3a12853c292815aaf2869174fa50b585c1ec3
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-10-05 23:38:56 +0530

    init

--- frontend/src/ethereum/whistler.js
+++
@@ -1,9 +0,0 @@
-import web3 from "./web3";
-import Manager from "./build/whistler.json";
-
-const instance = new web3.eth.Contract(
-  JSON.parse(Manager.interface),
-  "0xd2D51085b4095Da89027280a90537f17AbDd1400"
-);
-
-export default instance;
