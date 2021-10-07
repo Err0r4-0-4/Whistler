54f90f35fb612d20c8f991eb778838ffa46c0634
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-10-08 01:46:24 +0530

    error fix

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const fileuploader = require("express-fileupload");
 
 const app = express();
 
-app.use(cors());
 dotenv.config();
 app.use(fileuploader());
+app.use(cors());
 app.use(express.urlencoded());
 app.use(express.json());
 const PORT = process.env.PORT || 9000;
