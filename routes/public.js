593106ad2b654d9f41ee453f48e0d9b9149e9324
Jatin Gupta <201951073@iiitvadodara.ac.in>
2021-10-08 01:34:10 +0530

    ipfs added

--- routes/public.js
+++ routes/public.js
@@ -5,4 +5,5 @@ const publicController = require("../controllers/public");
 const route = express.Router();
 
 route.post("/login", publicController.login);
+route.post("/upload", publicController.uploadContract);
 module.exports = route;
